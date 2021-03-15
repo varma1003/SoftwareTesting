c449dc64412717f972cb7f2d3dde35477bc7dafa
SyamK <brucelee5243@gmail.com>
2021-03-15 20:07:17 +0100

    test class update

--- src/test/java/TestMainClass.java
+++ src/test/java/TestMainClass.java
@@ -5,26 +5,26 @@ import org.junit.Test;
 
 public class TestMainClass {
 
-    private static MainClass stest;
+    private static MainClass st;
 
     @BeforeClass
     public static void setup() {
-        stest =new MainClass();
+        st =new MainClass();
     }
 
     @Test
     //test by varma
     public void test_circle_area() {
-        assertEquals(50,stest.circle_area(4));
+        assertEquals(50,st.circle_area(4));
     }
 
     @Test
     public void test_rectangle_area() {
-        assertEquals(25,stest.rectangle_area(4,5));
+        assertEquals(25,st.rectangle_area(4,5));
     }
 
     @Test
     public void test_triangle_area() {
-        assertEquals(10,stest.triangle_area(4,5));
+        assertEquals(10,st.triangle_area(4,5));
     }
 }
